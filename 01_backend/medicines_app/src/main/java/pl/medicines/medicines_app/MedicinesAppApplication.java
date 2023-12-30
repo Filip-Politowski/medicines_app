e87baaebe70cc3eb41eac4dfcf0d37d771e4cf92
Filip Politowski <fil.politowski@gmail.com>
2023-12-30 18:34:58 +0100

    initial commit

---
+++ 01_backend/medicines_app/src/main/java/pl/medicines/medicines_app/MedicinesAppApplication.java
@@ -0,0 +1,13 @@
+package pl.medicines.medicines_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinesAppApplication.class, args);
+    }
+
+}
---
+++ 01_backend/medicines_app/src/test/java/pl/medicines/medicines_app/MedicinesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.medicines.medicines_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
